123ed0ff78ed54accc3c7b4e4bf7cb43a30be7f6
Connor Elsea <connorelsea@gmail.com>
2015-01-13 19:49:43 -0600

    Most of header CSS, added custom JS/Jquery

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+
+Custom Javascript by Elsea Labs
+
+See the source of the index.html page to
+view more information about the  company
+and its copyrights.
+
+*/
+
+/* On Resize */
+
+$(window).resize(function() {
+
+	// Set  the  height  of  the top  bar  and   the
+	// content  div's  distance  from the top of the
+	// page to the height of the logo, which changes
+	// automatically based on window size.
+
+	var logoHeight = $(".website-header .logo").css("height");
+	$(".website-header 	").css({
+		"height":logoHeight, "max-height":logoHeight, "min-height":logoHeight
+	});
+	$(".content").css("top", logoHeight);
+
+	console.log(logoHeight)
+
+});
