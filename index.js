cdbf8420d41e5e30cca2e35096bf6f07b2eda1b4
Connor Elsea <connorelsea@gmail.com>
2015-01-15 19:50:51 -0600

    Finished header re-sizing

--- index.js
+++ index.js
@@ -8,34 +8,58 @@ and its copyrights.
 
 */
 
-/* On Document Load */
+// On Document Load
 
 $(document).ready(function() {
 
-	// Perform the same changes done on resize when
-	// the page is initially loaded.
-	
-	var logoHeight = $(".website-header .logo").css("height");
-	$(".website-header 	").css({
-		"height":logoHeight, "max-height":logoHeight, "min-height":logoHeight
-	});
-	$(".content").css("top", logoHeight);
+	// Update the position of the header
+	positionHeader();
 
 });
 
-/* On Resize */
+// On Resize
 
 $(window).resize(function() {
+
+	// Update the position of the header
+	positionHeader();
+
+});
+
+// This function executes all  of  the  necessary
+// code for the header of the page to be re-sized
+
+function positionHeader() {
 
 	// Set  the  height  of  the top  bar  and   the
 	// content  div's  distance  from the top of the
 	// page to the height of the logo, which changes
 	// automatically based on window size.
 
-	var logoHeight = $(".website-header .logo").css("height");
+	var logoHeight = parseFloat($(".website-header .logo").css("height"));
+
 	$(".website-header 	").css({
 		"height":logoHeight, "max-height":logoHeight, "min-height":logoHeight
 	});
 	$(".content").css("top", logoHeight);
 
-});
+	// Set the size of the church logo elements
+
+	var chaliceHeight = logoHeight - 30.0;
+	var chaliceFromTop = parseFloat($(".website-header").css("height")) - chaliceHeight;
+
+	var wordHeight = logoHeight / 2;
+
+	$(".website-header .church-logo").css({
+		"height":chaliceHeight, "max-height":chaliceHeight, "min-height":chaliceHeight,
+		"top":chaliceFromTop
+	});
+
+	$(".website-header .church-name").css({ "top":wordHeight });
+
+	if (parseFloat($(".website-header").css("width")) < 600)
+		$(".website-header .church-name").css({ "display":"none" })
+	else
+		$(".website-header .church-name").fadeIn(2000);
+
+}
